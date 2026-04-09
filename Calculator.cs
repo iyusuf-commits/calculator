07a82c196917a9cd288a347d70b1acaca2746022
Imam Yusuf Akmal <i.yusuf@students.amikom.ac.id>
2026-04-09 15:43:36 +0700

    menambah fungsi

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+class calculator
+{
+	static void Main(string[] args)
+	{
+		
+	}
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+}
